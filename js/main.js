22355f61a09bae1885292963ae802803f2885db8
eirenna <rybalchenko.iryna@gmail.com>
2019-05-08 14:41:37 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,21 @@
+;
+(function () {
+	"use strict";
+	$(document).ready(function () {
+		$('.ba-text-slider').slick({
+			infinite: true,
+			//autoplay: true,
+			asNavFor: '.ba-img-slider',			
+			appendArrows: $('.ba-slider-arrows'),
+			prevArrow: '<button class="ba-btn"><svg class="icon icon-angle-left"><use xlink:href="#icon-angle-left"></use></svg></button>',
+			nextArrow: '<button class="ba-btn"><svg class="icon icon-angle-right"><use xlink:href="#icon-angle-right"></use></svg></button>'
+			
+		});
+		$('.ba-img-slider').slick({
+			infinite: true,
+			//autoplay: true,
+			asNavFor: '.ba-text-slider',
+			arrows: false			
+		});
+	});
+})();
